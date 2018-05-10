6a19919cb623413ba84a487c4f868b6fee749f86
Olivier Riccini <olivier-webdev@hotmail.com>
2018-05-10 17:58:35 -0400

    messages added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ console.log('Hello World from Webpacker')
 // import '../components/_custom_fullpage.js'
 
 
-
 // Selecting Navigation bar
 const navigation = document.querySelector('.navigation');
 
@@ -20,6 +19,7 @@ const navigation = document.querySelector('.navigation');
 const navLanding = document.getElementById('nav-landing');
 const navPhotos = document.getElementById('nav-photos');
 const navVideos = document.getElementById('nav-videos');
+const navInstagram = document.getElementById('nav-instagram');
 const navContact = document.getElementById('nav-contact');
 
 // Selecting all sections
@@ -29,6 +29,7 @@ const allSections = document.querySelectorAll('.section-slidinhojs');
 const sectionLanding = document.querySelector('.landing-section');
 const sectionPhotos = document.querySelector('.photos-section');
 const sectionVideos = document.querySelector('.videos-section');
+const sectionInstagram = document.querySelector('.instagram-section');
 const sectionContact = document.querySelector('.contact-section');
 
 
@@ -37,30 +38,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
   sectionLandingImg.style.opacity = '1';
 });
 
-function navGoRight() {
-  navigation.classList.remove('go-left');
-  navigation.classList.add('go-right');
-}
+// function navGoRight() {
+//   navigation.classList.remove('go-left');
+//   navigation.classList.add('go-right');
+// }
 
-function navGoLeft() {
-  navigation.classList.remove('go-right');
-  navigation.classList.add('go-left');
-}
+// function navGoLeft() {
+//   navigation.classList.remove('go-right');
+//   navigation.classList.add('go-left');
+// }
 
 function displaySection(sectionName) {
   allSections.forEach(function(section) {
     section.classList.remove('section-active');
   });
-
+  // $('.photos-section').slideDown();
   sectionName.classList.add('section-active');
 }
 
-navLanding.addEventListener('click', navGoLeft);
-navPhotos.addEventListener('click', navGoRight);
-navVideos.addEventListener('click', navGoLeft);
-navContact.addEventListener('click', navGoRight);
+// navLanding.addEventListener('click', navGoLeft);
+// navPhotos.addEventListener('click', navGoRight);
+// navVideos.addEventListener('click', navGoLeft);
+// navContact.addEventListener('click', navGoRight);
 
 navLanding.addEventListener('click', function() { displaySection(sectionLanding) } );
 navPhotos.addEventListener('click', function() { displaySection(sectionPhotos) } );
 navVideos.addEventListener('click', function() { displaySection(sectionVideos) } );
+navInstagram.addEventListener('click', function() { displaySection(sectionInstagram) } );
 navContact.addEventListener('click', function() { displaySection(sectionContact) } );
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'damien_dashboard', to: "pages#dashboard"
+  resources :messages, only: [ :create, :destroy ] do
+    post '/check', to: 'messages#check!'
+    post '/uncheck', to: 'messages#uncheck!'
+  end
 end
