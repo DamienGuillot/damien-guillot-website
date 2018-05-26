9050f441534bbc6081ba449a34ea0887bb171c04
Olivier Riccini <olivier-webdev@hotmail.com>
2018-05-26 16:48:28 -0400

    working on photos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,57 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+////////////// SLIDER //////////////////
+
+const containerSlider = document.getElementById('slider-container');
+const photos = document.querySelectorAll('.column-img img');
+const photosSlider = document.querySelectorAll('.photo-slider');
+
+const rightArrow = document.getElementById('right-arrow');
+const leftArrow = document.getElementById('left-arrow');
+
+function displaySlider(photoClikedIndex) {
+  containerSlider.style.display = "flex";
+  console.log(photoClikedIndex);
+  photosSlider.forEach(function(photoSlider) {
+    if ( photoSlider.dataset.index == photoClikedIndex ) {
+       photoSlider.style.display = 'block';
+    } else {
+       photoSlider.style.display = 'none';
+    }
+  });
+
+  let i = 0;
+  function changePhoto(direction) {
+    i += direction
+
+    photosSlider.forEach(function(photoSlider) {
+      if ( photoSlider.dataset.index == photoClikedIndex + i ) {
+         photoSlider.style.display = 'block';
+      } else {
+         photoSlider.style.display = 'none';
+      }
+    });
+  };
+
+  rightArrow.addEventListener('click', function() {
+    changePhoto(+1);
+  });
+
+  leftArrow.addEventListener('click', function() {
+    changePhoto(-1);
+  });
+}
+
+photos.forEach(function(photo) {
+  photo.addEventListener('click', function() {
+    displaySlider(this.dataset.index);
+  });
+});
+
+
+
 console.log('Hello World from Webpacker')
-// import 'fullpage.js'
-// import '../components/_custom_fullpage.js'
 
 
 // Selecting Navigation bar
@@ -38,28 +86,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   sectionLandingImg.style.opacity = '1';
 });
 
-// function navGoRight() {
-//   navigation.classList.remove('go-left');
-//   navigation.classList.add('go-right');
-// }
-
-// function navGoLeft() {
-//   navigation.classList.remove('go-right');
-//   navigation.classList.add('go-left');
-// }
-
 function displaySection(sectionName) {
   allSections.forEach(function(section) {
     section.classList.remove('section-active');
   });
-  // $('.photos-section').slideDown();
   sectionName.classList.add('section-active');
 }
-
-// navLanding.addEventListener('click', navGoLeft);
-// navPhotos.addEventListener('click', navGoRight);
-// navVideos.addEventListener('click', navGoLeft);
-// navContact.addEventListener('click', navGoRight);
 
 if ( navLanding && navPhotos && navVideos && navInstagram && navContact) {
   navLanding.addEventListener('click', function() { displaySection(sectionLanding) } );
@@ -74,6 +106,26 @@ function reload() {
 }
 
 setInterval(reload, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
