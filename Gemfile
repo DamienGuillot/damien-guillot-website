2f6b783611eeb91a71ecdb21e652cb5a192f3214
Olivier Riccini <olivier-webdev@hotmail.com>
2018-06-09 16:33:33 -0400

    mailer finihsed

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'bootstrap-sass', '~> 3.3.7'
 gem 'font-awesome-sass', '~> 5.0.9'
 gem 'simple_form'
 
+# Test mailer
+gem "letter_opener", group: :development
 # Devise to put an admin
 gem 'devise'
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,15 @@
 class Message < ApplicationRecord
+  after_create :send_contact
 
   validates :name, presence: true
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :content, length: { minimum: 20, maximum: 500 }
+
+
+  private
+
+  def send_contact
+    MessageMailer.contact(self).deliver_now
+  end
 end
